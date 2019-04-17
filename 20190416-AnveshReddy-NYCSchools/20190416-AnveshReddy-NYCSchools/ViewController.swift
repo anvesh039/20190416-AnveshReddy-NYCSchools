881f5364e5037fb005c573a7437e049c7e624e15
Anvesh <anveshios03@gmail.com>
2019-04-16 23:10:01 -0400

    NYC Schools Initial Project Setup

---
+++ 20190416-AnveshReddy-NYCSchools/20190416-AnveshReddy-NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190416-AnveshReddy-NYCSchools
+//
+//  Created by Anvesh on 4/16/19.
+//  Copyright © 2019 Anvesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
